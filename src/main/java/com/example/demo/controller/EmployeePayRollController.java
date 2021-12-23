7e7db06f2e9d4c4993ceeb78a322bbc8c9ea74a4
Yogendra <you@example.com>
2021-12-23 21:11:44 +0530

    Added: Demonstration of Http methods using rest controller

--- src/main/java/com/example/demo/controller/EmployeePayRollController.java
+++ src/main/java/com/example/demo/controller/EmployeePayRollController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,58 +11,65 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Employee;
-import com.example.demo.service.EmployeeService;
+import com.example.demo.data.EmployeePayrollData;
+
 
 
 @RestController
-@RequestMapping("/payroll")
+@RequestMapping("/employeePayrollservice")
+/**
+ * Controller : EmployeePayRollController
+ * @author Yogendra Lalit Sharma
+ *
+ */
 public class EmployeePayRollController {
 	
 	/**
-	 * @Autowired:Create object of implemented class and inject it
+	 * Call Get method
+	 * @return : Http Status
 	 */
-	@Autowired
-	private EmployeeService employeeservice;
-	
-	/**
-	 * Call get method to get details from table
-	 * @param empId
-	 * @return
-	 */
-	@GetMapping("/details/{empId}")
-	public Employee getDetails(@PathVariable String empId) {
-		return employeeservice.getDetails(Long.parseLong(empId));
+	@RequestMapping(value = {"","/","/get"})
+	public ResponseEntity<String> getEmployeePayrollData(){
+		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
 	}
 	
 	/**
-	 * Call post method to add details in the table
-	 * @param employee
-	 * @return
+	 * Call Get method
+	 * @param empId : Employee Id
+	 * @return : Employee id of the employee
 	 */
-	@PostMapping("/postDetails")
-	public Employee addDetails(@RequestBody Employee employee) {
-		return employeeservice.addDetails(employee);
+	@GetMapping("/get/{empId}")
+	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId){
+		return new ResponseEntity<String>("Get Call Success for Id: "+ empId, HttpStatus.OK);
 	}
 	
 	/**
-	 * Call put method to edit details in the table
-	 * @param employee
-	 * @return
+	 * Call post method to add details
+	 * @param empPayrollDTO : Employee details id, name & salary
+	 * @return : id, name & salary
 	 */
-	@PutMapping("/edit")
-	public Employee editDetails(@RequestBody Employee employee) {
-		return employeeservice.editDetails(employee);
+	@PostMapping("/create")
+	public ResponseEntity<String> getEmployeePayrollData(@RequestBody EmployeePayrollData empPayrollData){
+		return new ResponseEntity<String>("Created Employee Payroll Data for: "+ empPayrollData, HttpStatus.OK);
 	}
 	
 	/**
-	 * Call delete method to remove details from table
-	 * @param empId
-	 * @return
+	 * Call put method to update details
+	 * @param empPayrollDTO : Employee details id, name & salary 
+	 * @return : id, name & salary
 	 */
-	@DeleteMapping("/details/{empId}")
-	public Employee deleteEmpId(@PathVariable String empId) {
-		return this.employeeservice.deleteEmpId(Long.parseLong(empId));
+	@PutMapping("/update")
+	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollData empPayrollDTO){
+		return new ResponseEntity<String>("Updated Employee Payroll Data for: "+ empPayrollDTO, HttpStatus.OK);
 	}
 	
+	/**
+	 * Call delete method to remove employee details
+	 * @param empId : Employee id
+	 * @return : Employee id which is deleted
+	 */
+	@DeleteMapping("/delete/{empId}")
+	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+		return new ResponseEntity<String>("Delete call success for id: "+ empId, HttpStatus.OK);
+	}
 }
