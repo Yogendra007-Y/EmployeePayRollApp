aa7e98cfceca2895dcd9be9bb11d9a8962a9d083
Yogendra <you@example.com>
2021-12-27 15:09:58 +0530

    ADDED : LomBok Library For Logging A File

--- src/main/java/com/example/demo/EmployeePayRollAppApplication.java
+++ src/main/java/com/example/demo/EmployeePayRollAppApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayRollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayRollAppApplication.class, args);
-
+		System.out.println("Welcome To Employee Payroll Application");
+		ApplicationContext context = SpringApplication.run(EmployeePayRollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
--- src/main/java/com/example/demo/dto/ResponseDTO.java
+++ src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO {
 
 	private String message;
@@ -9,27 +12,6 @@ public class ResponseDTO {
 		super();
 		this.message = message;
 		this.data = data;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	@Override
-	public String toString() {
-		return "ResponseDTO [message=" + message + ", data=" + data + "]";
 	}
 
 }
