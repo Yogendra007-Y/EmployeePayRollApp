c8284d75cdd5223581ee32d9046e78a186a9907e
Yogendra <you@example.com>
2021-12-27 16:05:27 +0530

    ADDED : Determine the Logging Levels, Logging

--- src/main/java/com/example/demo/EmployeePayRollAppApplication.java
+++ src/main/java/com/example/demo/EmployeePayRollAppApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
