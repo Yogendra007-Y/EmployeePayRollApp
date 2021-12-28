1e3a634fa2a05e8ea2f9505fff6294d37bbeafb4
Yogendra <you@example.com>
2021-12-28 18:24:47 +0530

    ADDED : Validation On EmpPayrollDTO And Add Exception In Exception Handler Class

--- src/main/java/com/example/demo/exceptions/EmployeePayrollExceptionalHandler.java
+++ src/main/java/com/example/demo/exceptions/EmployeePayrollExceptionalHandler.java
@@ -1,43 +1,61 @@
 package com.example.demo.exceptions;
 
 import java.util.List;
+
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.demo.controller.EmployeePayrollController;
 import com.example.demo.dto.ResponseDTO;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionalHandler {
-	/**
-	 * Method to handle any Exception thrown
-	 * @param exception
-	 * @return : ResponseEntity of Exception
-	 */
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ResponseDTO> handlerMethodArgumentNotValidException(
-			MethodArgumentNotValidException exception) {
-		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
-		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request ", errMsg);
+	private static final String message = "Exception While Processing REST Request";
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handelHttpMessageNotReadableException(
+			HttpMessageNotReadableException exception) {
+		log.error("Invalid Date Format", exception);
+		ResponseDTO responseDTO = new ResponseDTO(message, "Should have date in dd MMM yyyy format");
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 	}
 
 	/**
-	 * Method to handle when user Id not found
-	 * @param exception
-	 * @return : Response Entity of Exception
+	 * handle MethodArgumentNotValid exceptions.
+	 * 
+	 * @param : exception
+	 * @return : ResponseEntity of Exception and HttpStatus
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO(message, errMesg);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+
+	/**
+	 * handle when user Id not found
+	 * 
+	 * @return : ResponseEntity of Exception and HttpStatus
 	 */
 	@ExceptionHandler(EmployeePayrollException.class)
-	public ResponseEntity<ResponseDTO> handlerEmployeePayrollException(EmployeePayrollException exception) {
-		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request ", exception.getMessage());
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception) {
+		ResponseDTO responseDTO = new ResponseDTO(message, exception.getMessage());
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 	}
 
